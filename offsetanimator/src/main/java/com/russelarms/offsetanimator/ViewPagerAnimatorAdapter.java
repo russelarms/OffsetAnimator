b908d504d69ac7e301b06a808ee214f9ab7661f6
Ruslan Urmeev <russel.arms@gmail.com>
2017-05-17 19:49:53 +0400

    issue #1 fix. mutual exceptions

--- offsetanimator/src/main/java/com/russelarms/offsetanimator/ViewPagerAnimatorAdapter.java
+++ offsetanimator/src/main/java/com/russelarms/offsetanimator/ViewPagerAnimatorAdapter.java
@@ -8,29 +8,31 @@ import android.util.Log;
 public class ViewPagerAnimatorAdapter {
 
     private final Scene scene;
-    private int prevPosition;
+    private int prevPageNumber;
 
     public ViewPagerAnimatorAdapter(Scene scene) {
         this.scene = scene;
     }
 
-    public void onPageScrolled(int position, float positionOffset) {
+    /**
+     * @param pageNumber     - takes values from 0 and higher
+     * @param positionOffset - takes values from 0.0 to 0.9999+
+     */
+    public void onPageScrolled(int pageNumber, float positionOffset) {
         try {
             if (!scene.isInitialized()) {
                 return;
             }
 
-            if (positionOffset == 0.0f && prevPosition != position) {
-                positionOffset = 1f;
+            if (positionOffset > 0.0f || pageNumber == prevPageNumber) {
+                scene.act(pageNumber, positionOffset);
+            } else {
+                scene.act(prevPageNumber, 1f);
             }
 
-            if (position == prevPosition) {
-                scene.act(position, positionOffset);
-            }
-
-            prevPosition = position;
+            prevPageNumber = pageNumber;
         } catch (RuntimeException e) {
-            String where = "position is " + position + " previous position is " + prevPosition +
+            String where = "position is " + pageNumber + " previous position is " + prevPageNumber +
                     " position offset is " + positionOffset + "\n cause: " + e;
             Log.e(ViewPagerAnimatorAdapter.class.getCanonicalName(), where);
             throw e;
